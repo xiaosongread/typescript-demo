c801fc087199c61714a1d17e69b38dc712abb6ba
xiaosongread <598349146@qq.com>
2019-07-08 19:55:25 +0800

    fanxing

--- src/test/function.ts
+++ src/test/function.ts
@@ -69,7 +69,64 @@
 // let cardPicker = deck.createCardPicker();
 // let pickedCard = cardPicker();
 
-// alert("card: " + pickedCard.card + " of " + pickedCard.suit);
+// console.log("card: " + pickedCard.card + " of " + pickedCard.suit);
+
+// 重载
+// JavaScript里函数根据传入不同的参数而返回不同类型的数据是很常见的
+// let suits = ["hearts", "spades", "clubs", "diamonds"];
+
+// function pickCard(x): any {
+//     // Check to see if we're working with an object/array
+//     // if so, they gave us the deck and we'll pick the card
+//     if (typeof x == "object") {
+//         let pickedCard = Math.floor(Math.random() * x.length);
+//         return pickedCard;
+//     }
+//     // Otherwise just let them pick the card
+//     else if (typeof x == "number") {
+//         let pickedSuit = Math.floor(x / 13);
+//         return { suit: suits[pickedSuit], card: x % 13 };
+//     }
+// }
+
+// let myDeck = [{ suit: "diamonds", card: 2 }, { suit: "spades", card: 10 }, { suit: "hearts", card: 4 }];
+// let pickedCard1 = myDeck[pickCard(myDeck)];
+// alert("card: " + pickedCard1.card + " of " + pickedCard1.suit);
+
+// let pickedCard2 = pickCard(15);
+// alert("card: " + pickedCard2.card + " of " + pickedCard2.suit);
+// pickCard方法根据传入参数的不同会返回两种不同的类型。 如果传入的是代表纸牌的对象，函数作用是从中抓一张牌。 如果用户想抓牌，我们告诉他抓到了什么牌。 但是这怎么在类型系统里表示呢。
+
+// > 方法是为同一个函数提供多个函数类型定义来进行函数重载。 编译器会根据这个列表去处理函数的调用。 下面我们来重载pickCard函数
+
+// let suits = ["hearts", "spades", "clubs", "diamonds"];
+
+// function pickCard(x: {suit: string; card: number; }[]): number;
+// function pickCard(x: number): {suit: string; card: number; };
+// function pickCard(x): any {
+//     // Check to see if we're working with an object/array
+//     // if so, they gave us the deck and we'll pick the card
+//     if (typeof x == "object") {
+//         let pickedCard = Math.floor(Math.random() * x.length);
+//         return pickedCard;
+//     }
+//     // Otherwise just let them pick the card
+//     else if (typeof x == "number") {
+//         let pickedSuit = Math.floor(x / 13);
+//         return { suit: suits[pickedSuit], card: x % 13 };
+//     }
+// }
+
+// let myDeck = [{ suit: "diamonds", card: 2 }, { suit: "spades", card: 10 }, { suit: "hearts", card: 4 }];
+// let pickedCard1 = myDeck[pickCard(myDeck)];
+// alert("card: " + pickedCard1.card + " of " + pickedCard1.suit);
+
+// let pickedCard2 = pickCard(15);
+// alert("card: " + pickedCard2.card + " of " + pickedCard2.suit);
+
+
+
+
 
 
 

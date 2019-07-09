9bab231672944ac248a18064cb4024d6dc2521e7
xiaosongread <598349146@qq.com>
2019-07-09 16:03:46 +0800

    commit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # typescript-demo
+
+## ts-node 文件名.ts // 运行ts文件
+## tsc 文件名.ts // 打包转化ts文件为js文件
--- src/test/类型推论.ts
+++ src/test/类型推论.ts
@@ -1,3 +1,7 @@
 // let x = 3
 // x = '123' // 不能将类型“"123"”分配给类型“number”
 
+window.onmousedown = function(mouseEvent) {
+  console.log(mouseEvent.button);   //<- OK
+  // console.log(mouseEvent.kangaroo); //<- Error!
+};
